ebb2ce6fdd2109961cd1c56eff594a69d03df0ce
mdiibrahim <mdiibrahim549@gmail.com>
2022-11-11 23:27:39 +0600

    finish

--- index.js
+++ index.js
@@ -9,55 +9,52 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@rhidys-cook-book.ms5fwvj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try {
-      const serviceCollection = client.db("Rhidys-Cook-Book").collection("services");
-        app.get('/services', async (req, res) => {
-          const cursor = serviceCollection.find({});
-          const services = await cursor.toArray()
-          res.send(services)
-     })
-        app.get('/services-at-home', async (req, res) => {
-          const cursor = serviceCollection.find({});
-          const serviceAtHomes = await cursor.skip(3).limit(3).toArray();
-          res.send(serviceAtHomes)
-     })
-      app.get('/services/:id', async (req, res) => {
-        const id = req.params.id;
-          const query = {_id: ObjectId(id)}
-          const service = await serviceCollection.findOne(query)
-          res.send(service)
-     })
-      app.get('/:id', async (req, res) => {
-        const id = req.params.id;
-          const query = {_id: ObjectId(id)}
-          const service = await serviceCollection.findOne(query)
-          res.send(service)
-      })
-      const reviewCollection = client.db('Rhidys-Cook-Book').collection('reviews');
-      app.post('/reviews', async (req, res) => {
-        const review = req.body;
-        const result = await reviewCollection.insertOne(review);
-        res.send(result);
-      });
-      app.get('/reviews', async (req, res) => {
-
-        
-        
-        const cursor = reviewCollection.find({});
-        const reviews = await cursor.toArray();
-        res.send(reviews);
+  try {
+    const serviceCollection = client.db("Rhidys-Cook-Book").collection("services");
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray()
+      res.send(services)
+    })
+    app.get('/services-at-home', async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const serviceAtHomes = await cursor.skip(3).limit(3).toArray();
+      res.send(serviceAtHomes)
+    })
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const service = await serviceCollection.findOne(query)
+      res.send(service)
+    })
+    app.get('/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const service = await serviceCollection.findOne(query)
+      res.send(service)
+    })
+    const reviewCollection = client.db("Rhidys-Cook-Book").collection("reviews");
+    app.post('/reviews', async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
     });
-    } 
-    finally {
-      
-    }
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
   }
-  run().catch(error => console.error(error));
+  finally {
+
+  }
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
-    res.send("Rhidys-Cook-Book server is running..........");
+  res.send("Rhidys-Cook-Book server is running..........");
 })
 
 app.listen(port, () => {
-    console.log(`Rhidys-Cook-Book Server is running on ${port}`);
+  console.log(`Rhidys-Cook-Book Server is running on ${port}`);
 });
